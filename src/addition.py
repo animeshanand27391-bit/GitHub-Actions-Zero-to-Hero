9de896ac1023afa224d8691e677bbad37ea458ea
Animesh Anand <animesh.anand27391@gmail.com>
2026-05-17 09:39:49 -0700

    trigger actions

--- src/addition.py
+++ src/addition.py
@@ -4,7 +4,7 @@ def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 3) == 3
+    assert add(1, 3) == 4
     assert add(1, -1) == 0
     assert add(0, 0) == 0
     assert add(-1, -1) == -2
